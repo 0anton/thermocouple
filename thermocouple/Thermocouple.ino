429ec6dd13b000d432489d13aeb4ce8f1ff2d81a
Anton Golubev <Anton Golubev@x230-PC.sbor>
2017-02-09 01:02:44 +0300

    add low power support

--- thermocouple/Thermocouple.ino
+++ thermocouple/Thermocouple.ino
@@ -1,38 +1,22 @@
 #include <SPI.h>
 #include "Adafruit_MAX31855.h"
+#include "LowPower.h"
 
 // (c) Anton Golubev 2017
 
-// Default connection is using software SPI, but comment and uncomment one of
-// the two examples below to switch between software SPI and hardware SPI:
-
-// Example creating a thermocouple instance with software SPI on any three
-// digital IO pins.
-//#define MAXDO   3
-//#define MAXCS   4
-//#define MAXCLK  5
-//
-//// initialize the Thermocouple
-//Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
-
-// Example creating a thermocouple instance with hardware SPI
-// on a given CS pin.
 #define MAXCS   10
 Adafruit_MAX31855 thermocouple(MAXCS);
 
 void setup() {
-  while (!Serial); // wait for Serial on Leonardo/Zero, etc
+  while (!Serial);
 
   Serial.begin(115200);
 
   Serial.println("MAX31855 test");
-  Serial.println("MAX31855 test 2");
-  // wait for MAX chip to stabilize
-  delay(500);
 }
 
 void loop() {
-  // basic readout test, just print the current temp
+	LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
    Serial.print("Internal Temp = ");
    Serial.println(thermocouple.readInternal());
 
