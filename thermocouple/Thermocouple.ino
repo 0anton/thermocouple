bd491c1b8b003d3c00d266d831c01c28d109f806
Anton Golubev <Anton Golubev@x230-PC.sbor>
2017-02-09 02:07:20 +0300

    - add low power mode and lcd code

--- thermocouple/Thermocouple.ino
+++ thermocouple/Thermocouple.ino
@@ -1,6 +1,12 @@
 #include <SPI.h>
 #include "Adafruit_MAX31855.h"
 #include "LowPower.h"
+#include "PCD8544_SPI.h"
+
+#define USE_FRAME_BUFFER
+
+PCD8544_SPI_FB lcd;
+
 
 // (c) Anton Golubev 2017
 
@@ -9,26 +15,36 @@ Adafruit_MAX31855 thermocouple(MAXCS);
 
 void setup() {
   while (!Serial);
-
   Serial.begin(115200);
-
   Serial.println("MAX31855 test");
+  lcd.begin();
+  lcd.print(F("Welcome to thermosensor"));
+  lcd.renderAll();
+  LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
 }
 
 void loop() {
-	LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+	size_t len=0;
+
+	LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+	lcd.clear();
+
    Serial.print("Internal Temp = ");
    Serial.println(thermocouple.readInternal());
+
+   len += lcd.print(F("Internal Temp = "));
+   len += lcd.println(thermocouple.readInternal());
 
    double c = thermocouple.readCelsius();
    if (isnan(c)) {
      Serial.println("Something wrong with thermocouple!");
+     len += lcd.print(F("Something wrong with thermocouple!"));
    } else {
      Serial.print("C = ");
+     len += lcd.print(F("C = "));
      Serial.println(c);
+     len += lcd.println(c);
    }
-   //Serial.print("F = ");
-   //Serial.println(thermocouple.readFarenheit());
 
-   delay(1000);
+   lcd.renderString(0, 0, len);
 }
